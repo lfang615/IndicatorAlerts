2e4c3cea784be48ebfd87c6e1676b55e43e75bb8
Larry Fang <lfang615@gmail.com>
2018-11-19 21:37:41 -0600

    added volume class

---
+++ volume_module.js
@@ -0,0 +1,98 @@
+const api = require('binance');
+
+class Volume{
+    constructor(pair){
+        this.pair = pair;
+        this.exchangeRest = new api.BinanceRest({
+            key: 'nDvUlZPkI0S1adgfcR4eE7C5BB23lcFaLKV1LL7SYZxdhHYal0inX7NBwE4k7OKZ', 
+            secret: 'e5lCovFNHjMS98tRQCNPHtBUC6baCgSwOQMug7JyJQ8DaNBGElNln2woyfJoNAyO'
+        })
+    }
+
+    getVolumeAvg(interval, periodLength){
+        this.exchangeRest.klines({
+            symbol: this.pair,
+            interval: interval,
+            limit: periodLength
+
+        })
+        .then((result) => {
+            let sum = 0;
+            for(let i = 0; i < result.length; i++){
+                sum += parseFloat(result[i]['volume']);
+            }
+            console.log('Average volumne for the 20 day period: ' + 
+            Math.round(sum / periodLength).toString());
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    getPrevClosePrice(interval){
+        this.exchangeRest.klines({
+            symbol: this.pair,
+            interval: interval,
+            limit: 2
+        })
+        .then((result) => {
+            console.log('Previous closing price: ' + result[0]['close']);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    calcRSI(interval){
+        this.exchangeRest.klines({
+            symbol: this.pair,
+            interval: interval,
+            limit: 14 
+        })
+        .then((result) => {
+           this.rsiDebug(result);
+        }).catch((err) => {console.log(err);})
+    }
+
+    rsiDebug(result){
+        let gain = 0;
+        let loss = 0
+        let prevGain = 0;
+        let prevLoss = 0;
+
+        for(let i = 0; i < result.length; i++){
+            for(let z = i+1; z <= result.length - 1; z++){
+                let change = this.closingPriceChange(parseFloat(result[i]['close']), parseFloat(result[z]['close'])); 
+                if(change >= 0){
+                    gain += change;
+                }
+                if(change < 0){
+                    loss += (-1 * change);
+                }
+               
+                break;
+            }
+        }
+        
+        let current = parseFloat(result[13]['close']) - parseFloat(result[12]['close']);
+        let close = parseFloat(result[13]['close']);
+        let alpha = 1 / 14;
+        let smoothedRS = ((gain * 13) + ((current) >= 0 ? current : 0) / 14) / ((loss * 13) + ((current) < 0 ? (-1 * current) : 0) / 14); 
+        let new_avg_value = close * alpha + (1 - alpha) * smoothedRS;
+        let avgGain = gain / 14;
+        let avgLoss = loss / 14;
+        
+        let rs = avgGain / avgLoss;
+        let rsi = 100 - (100 / (1 + rs));
+
+        let newRSI = 100 - (100 / (1 + new_avg_value));
+
+        console.log(newRSI);
+    }
+
+    closingPriceChange(prev, next){
+        return next - prev;
+    }
+}
+
+module.exports = Volume;
